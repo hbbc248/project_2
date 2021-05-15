03fa923aec2ca9159457cef8ffe69d8b1f5ad7ca
Ibrahim <ibrahimzerlin@hotmail.com>
2021-05-14 21:14:08 -0500

    initial commit

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,57 @@
+const router = require('express').Router();
+const { User } = require("../../models");
+
+
+
+// GET /api/users
+router.get("/", (req, res) => {
+    // Access our User model and run .findAll() method)
+    User.findAll({
+      attributes: { exclude: ["password"] },
+    })
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
+
+// GET /api/users/id
+
+
+
+// POST /api/users
+router.post("/", (req, res) => {
+    // expects {username: 'Jose', password: 'password1234'}
+    User.create({
+      username: req.body.username,
+      password: req.body.password,
+    }).then((dbUserData) => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+  
+        res.json(dbUserData);
+        timeout();
+      });
+    });
+});
+
+// LOG IN GOES IN HERE
+
+
+// LOG OUT GOES IN HERE
+
+
+
+// PUT /api/users/1
+
+
+// DELETE /api/users/1
+
+
+
+
+
+module.exports = router;
---
+++ controllers/home-routes.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { User } = require("../models");
+
+// routes go in here
+
+
+
+module.exports = router;
